09710b7a65ba1d3d7ff63d0f2ac6912445dec852
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2021-10-03 18:27:18 +0530

    use other components props to our component done

---
+++ src/components/html/CustomComponent.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Greet from "../Greet";
+
+/**
+ * use props of other components
+ */
+export const CustomComponent = (props: React.ComponentProps<typeof Greet>) => {
+  return <div>{console.log("props", props)}</div>;
+};
